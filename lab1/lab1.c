3b4aa78de6846492429b998845a798df069a9c64
Sal1x <mr.verbovoy@mail.ru>
2021-03-25 22:29:21 +0300

    delete copy to user

--- lab1/lab1.c
+++ lab1/lab1.c
@@ -125,10 +125,6 @@ static ssize_t dev_read(struct file* file, char __user* ubuf, size_t count, loff
 	{
 		return 0;
 	}
-	if (copy_to_user(ubuf, THIS_MODULE->name, len) != 0) // todo change this 
-	{
-		return -EFAULT;
-	}
 	*ppos += len;
 
 	kfree(local_buf);
